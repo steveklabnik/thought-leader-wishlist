a8e51f70f8045184900dd75f94dff686e9d6a9e5
Robert Slifka <robert.slifka@gmail.com>
2020-12-06 17:34:21 -0800

    Backfill tests for RollSet

--- spec/lib/roll_set_spec.rb
+++ spec/lib/roll_set_spec.rb
@@ -1,46 +1,77 @@
 require 'roll_set'
 
 describe RollSet do
-  let(:traits) {
+  subject {
+    RollSet.new('PvP', 2050789284, traits, roll_data)
+  }
+
+  let(:traits)    {
     {
-      'barrels'      => {'t' => 9, 'c' => 2},
-      'magazines'    => {'t' => 8, 'c' => 2},
-      'perks1'       => {'t' => 6, 'c' => 1},
-      'perks2'       => {'t' => 6, 'c' => 1},
-      'masterworks'  => {'t' => 4, 'c' => 1}
+      'barrels' =>     {'t' => 9, 'c' => 2},
+      'magazines' =>   {'t' => 7, 'c' => 2},
+      'perks1' =>      {'t' => 12, 'c' => 1},
+      'perks2' =>      {'t' => 12, 'c' => 1},
+      'masterworks' => {'t' => 4, 'c' => 1}
     }
   }
+
   let(:roll_data) {
     {
-      'name' => 'Full Auto Super Shredder',
+      'name' => 'Vertical/Dark Side',
       'perks' => {
         'base' => {
-          'barrels' => ['Polygonal Rifling', 'Chambered Compensator', 'Smallbore', 'Corkscrew Rifling', 'Hammer-Forged Rifling'],
-          'magazines' => ['Accurized Rounds', 'Steady Rounds', 'Flared Magwell', 'Tactical Mag'],
-          'perks1' => ['Full Auto Trigger System'],
-          'perks2' => ['Vorpal'],
-          'masterworks' => ['Stability MW', 'Range MW']
+          'barrels' => ['Arrowhead Brake'],
+          'magazines' => ['Steady Rounds'],
+          'perks1' => ['Killing Wind'],
+          'perks2' => ['Headseeker', 'Moving Target', 'Kill Clip', 'Eye of the Storm'],
+          'masterworks' => ['Stability MW']
         },
         'extensions' => { 
-
+          'magazines' => ['Steady Rounds', 'Flared Magwell', 'Tactical Mag'] 
         }
       },
-      'variants' => [
-        '💥💥💥🌟',
-        '💥💥💥 (-masterworks)'
-      ]
+      # Level of indirection that allows locally-scoped 'let' blocks specify
+      # roll variants per example
+      'variants' => get_variants()
     }
   }
 
-  subject(:si2) {
-    RollSet.new('PvP', 3937866388, traits, roll_data)
-  }
+  def get_variants
+    variants
+  end
 
   describe 'wishlist formatting' do
-    it 'outputs comments in the correct format with correct probabilities' do
-      gold_output = IO.read('./spec/gold_data/roll_set_data.txt')
-      expect(subject.generate_wishlist_txt()).to eq(gold_output)
+
+    context 'when all base perks are used' do
+      let(:variants) {
+        ["🏔🌒🌒🌒🌟 Collector's Edition"]
+      }
+      it 'generates a wishlist' do
+        gold_output = IO.read('./spec/gold_data/roll_set/when_all_base_perks_are_used_data.txt')
+        expect(subject.generate_wishlist_txt()).to eq(gold_output)
+      end
     end
+    
+    context 'when some base perks are not used' do
+      let(:variants) {
+        ["🏔🌒🌒 (-magazines, -masterworks)"]
+      }
+      it 'generates a wishlist' do
+        gold_output = IO.read('./spec/gold_data/roll_set/when_some_base_perks_are_not_used_data.txt')
+        expect(subject.generate_wishlist_txt()).to eq(gold_output)
+      end
+    end
+
+    context 'when additional perks are used' do
+      let(:variants) {
+        ["🏔🌒🌒🌒 CE (+magazines)"]
+      }
+      it 'generates a wishlist' do
+        gold_output = IO.read('./spec/gold_data/roll_set/when_additional_perks_are_used_data.txt')
+        expect(subject.generate_wishlist_txt()).to eq(gold_output)
+      end
+    end
+
   end
 
 end
